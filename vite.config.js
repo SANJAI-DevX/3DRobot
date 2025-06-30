2f3aea37cc3a15006aad7b4cb3df3ab8eff4f8e9
SANJAI <sanjai934248@gmail.com>
2025-07-01 00:55:09 +0530

    new

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  },
+  optimizeDeps: {
+    include: ['tailwindcss', 'autoprefixer']
+  }
+})
